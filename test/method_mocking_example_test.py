f4690a23fb8e26f02f81747273c8329bffc8cf19
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2018-07-27 15:02:59 -0400

    Use pytest syntax

--- test/method_mocking_example_test.py
+++ test/method_mocking_example_test.py
@@ -1,5 +1,6 @@
 import unittest.mock
 
+
 class Callee:
     def __init__(self, member):
         self.member = member
@@ -7,16 +8,23 @@ class Callee:
     def do_something(self, method_argument):
         pass
 
+
 def do_call():
     Callee("Bar").do_something("Foo")
 
+
 mocked_do_something_method = unittest.mock.Mock(wraps=Callee.do_something)
 
-with unittest.mock.patch.object(Callee, 'do_something', new=lambda *args, **kwargs: mocked_do_something_method(*args, **kwargs)):
+with unittest.mock.patch.object(
+        Callee,
+        'do_something',
+        new=lambda *args, **kwargs: mocked_do_something_method(*args, **kwargs)):
     do_call()
 
-assert mocked_do_something_method.call_count == 1
-method_args, method_kwargs = mocked_do_something_method.call_args
-callee_self, callee_method_argument = method_args
-assert callee_self.member == "Bar"
-assert callee_method_argument == "Foo"
+
+def test_method_mocking():
+    assert mocked_do_something_method.call_count == 1
+    method_args, method_kwargs = mocked_do_something_method.call_args
+    callee_self, callee_method_argument = method_args
+    assert callee_self.member == "Bar"
+    assert callee_method_argument == "Foo"
